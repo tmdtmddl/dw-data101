6f08dd4d6e86732b433dc8636b9cdef64644df10
tmdtmddl <tmddlqkqh@naver.com>
2024-12-12 17:28:49 +0900

    commit message

---
+++ 24_12_11/index.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById("modal");
+
+// 실행하고 싶은 동작
+function show() {
+  const modal = document.getElementById("modal");
+  modal.style = "display:flex";
+}
+
+// 끄는동작
+function hide() {
+  const modal = document.getElementById("modal");
+  modal.style = "display:none";
+}
